2852e13bd999fc83e743b7a253e375b6e2b7e812
nikhil <nihkilmogre1998@gmail.com>
2024-11-03 00:48:56 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'Nikhil Mogre',
+    author_email= 'nikhilmogre1998@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
